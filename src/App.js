f5c937d6345603344404d7c1d08a4480b6ea82c2
abachant <gbachant@gmail.com>
2020-04-30 21:36:01 -0400

    Add basic Station component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Station from './Station'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Station name="Park Street"/>
+      <Station name="Tremon Street" />
     </div>
   );
 }
---
+++ src/Station.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Station(props) {
+  return (
+    <div className="station">
+        <svg height="50" width="50">
+            <circle cx="25" cy="25" r="5" stroke="black" stroke-width="3" fill="black" />
+        </svg>
+        <h2 className="station-name">{props.name}</h2>
+    </div>
+  );
+}
+
+export default Station;
